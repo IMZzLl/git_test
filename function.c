1f8f915a8fab13a4af5ddc36d05fc27eca48a6f7
LiZhe <2241876@s.hlju.edu.cn>
2026-04-10 21:02:16 +0800

    fff

--- function.c
+++ function.c
@@ -4,3 +4,7 @@ void add(int a, int b)
 {
     printf("The sum of %d and %d is %d\n", a, b, a + b);
 }
+void subtract(int a, int b)
+{
+    printf("The difference of %d and %d is %d\n", a, b, a - b);
+}
--- test.c
+++ test.c
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
     printf("Hello, World!\n");
     printf("This is a test program.\n");
     add(5, 10);
+    subtract(10, 5);
     return 0;
 }
