c4df2d77b7f6000d996a61af041f43364f12716f
LiZhe <2241876@s.hlju.edu.cn>
2026-04-10 20:59:02 +0800

    fff

---
+++ function.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void add(int a, int b)
+{
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+}
--- test.c
+++ test.c
@@ -4,5 +4,6 @@ int main(int argc, char const *argv[])
 {
     printf("Hello, World!\n");
     printf("This is a test program.\n");
+    add(5, 10);
     return 0;
 }
